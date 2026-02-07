cf1d4d25f96c02b1f00eb30b8fb3cd719d6c54a6
Numan Karaaslan <numan4634@hotmail.com>
2026-02-07 21:31:33 +0300

    Update background.js

--- background.js
+++ background.js
@@ -1,16 +1,18 @@
-chrome.action.onClicked.addListener((tab) => {
-  const PANEL_WIDTH = 450;
-
-  chrome.windows.getCurrent({}, (currentWindow) => {
-    chrome.windows.create({
-      url: chrome.runtime.getURL(
-        "sidepanel.html?tabId=" + tab.id
-      ),
-      type: "popup",
-      width: PANEL_WIDTH,
-      height: currentWindow.height,
-      left: currentWindow.left + currentWindow.width - PANEL_WIDTH,
-      top: currentWindow.top
-    });
-  });
-});
+{
+  "manifest_version": 3,
+  "name": "Page Summarizer",
+  "version": "1.0",
+  "permissions": [
+    "scripting",
+    "tabs"
+  ],
+  "host_permissions": [
+    "<all_urls>"
+  ],
+  "background": {
+    "service_worker": "background.js"
+  },
+  "action": {
+    "default_title": "Open Summarizer"
+  }
+}
